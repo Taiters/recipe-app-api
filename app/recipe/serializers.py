ecf7ac9461f5e2340fb8c233eb03dafc88fa7266
Dan <daniel.tait@travelperk.com>
2023-11-09 16:42:05 +0000

    Add ingredient listing api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,8 +1,18 @@
 """
 Serializers for recipe APIs
 """
-from core.models import Recipe, Tag
 from rest_framework import serializers
+
+from core.models import Ingredient, Recipe, Tag
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredients."""
+
+    class Meta:
+        model = Ingredient
+        fields = ["id", "name"]
+        read_only_fields = ["id"]
 
 
 class TagSerializer(serializers.ModelSerializer):
