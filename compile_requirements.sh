e26960a8c8c3b05cde49ec3b3b18582403b6dc45
Dan <daniel.tait@travelperk.com>
2023-11-16 14:46:17 +0000

    Compile requirements

---
+++ compile_requirements.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+IMAGE_NAME="recipe-app-api/pip-compile"
+MOUNT_PATH="/opt/requirements"
+
+# Check if Docker is installed and running
+if ! command -v docker &> /dev/null
+then
+    echo "Docker not available. Start docker daemon"
+    exit 1
+fi
+
+docker build -t $IMAGE_NAME -f Dockerfile-pip-compile .
+
+docker run \
+    --mount "source=${PWD},target=${MOUNT_PATH},type=bind" \
+    $IMAGE_NAME --verbose --no-emit-index-url requirements.in
+
+docker run \
+    --mount "source=${PWD},target=${MOUNT_PATH},type=bind" \
+    $IMAGE_NAME --verbose --no-emit-index-url requirements.dev.in
